2bb1c7a4afe3feea8f8dcf4845b7b678f40c8745
Fabricio Emder <elector102@gmail.com>
2018-08-16 17:00:15 -0300

    - Change motor name or identification, instead to use D(Delantero) now I am using front. - Change motor name or identification, instead to use T(Tracero) now I am using back. - Add PID control for the motor. - Add posibility to change PID tune from MatLab. - Add convertion to cm/s and from cm/s to PID control and parameters. - Add simulink program to control 4 motor from linear vel and angular vel.

--- arduino/brushless_motor_controller/controller/src/main.cpp
+++ arduino/brushless_motor_controller/controller/src/main.cpp
@@ -2,65 +2,86 @@
 #include <DueTimer.h>
 #include "pwm_lib.h"
 #include "tc_lib.h"
+#include <PID_v1.h>
 
 void funcion_t1();
 
 #define PWM_PERIODO_US 10000 //10khz
-
+#define DEBUG_MODE true
 // Diametro de la rueda en cm. cm = 2.54 * pulgadas
-#define diametro_cm 25.4 // 10 pulgadas
-#define pi 3.1415926535
+#define DIAMETRO_CM 24.13 // 9.5 pulgadas
+#define PI 3.1415926535
 // cantidad de periodos que se tienen por vuelta del motor
-#define pulsos_por_vuelta 21
+#define PULSOS_POR_VUELTAS 42
 // perimetro en cm de la rueda
-float perimetro_rueda = pi * diametro_cm;
+double perimetro_rueda = PI * DIAMETRO_CM;
 // distancia recorrida por cada periodo de señal del motor
-float distancia_periodo = perimetro_rueda / pulsos_por_vuelta;
-#define us_to_s 1/1000000
-#define cm_to_m 1/100
-#define metros_en_us distancia_periodo * cm_to_m / us_to_s
+double distancia_periodo = perimetro_rueda / PULSOS_POR_VUELTAS;
+double us_to_s = 1000000;
+double cm_to_m = 1;
+double metros_en_us = distancia_periodo * cm_to_m * us_to_s;
+
+#define MAX_VEL_CM_S 1000
+
+// Sentido de giro del motor
+#define z_f_pin_motor_front 9
+// Pulsos de velocidad de salida
+//#define signal_pin_motor_front 10
+// Enable control
+#define el_pin_motor_front 12
 
 
-// Definiciones motor D(Delantero)
+// Definiciones motor front(Delantero)
 // Defining pwm object using pin 6, pin PC24 mapped to pin 6 on the DUE
 // This object uses PWM channel 0
 #define CAPTURE_TIME_WINDOW 40000000 // usecs
-arduino_due::pwm_lib::pwm<arduino_due::pwm_lib::pwm_pin::PWML5_PC22> pwm_motor_D;
+arduino_due::pwm_lib::pwm<arduino_due::pwm_lib::pwm_pin::PWML5_PC22> pwm_motor_front;
 
-int salida_pwm_motor_D = 0;
-int entrada_analogica_motor_D = 0;
+int salida_pwm_motor_front = 0;
+int entrada_analogica_motor_front = 0;
 
 capture_tc8_declaration(); // TC0 and channel 1 pin A7
-auto& capture_motor_D = capture_tc8;
+auto& capture_motor_front = capture_tc8;
 
-uint32_t status_motor_D, duty_motor_D, period_motor_D, period_motor_D_us, pulses_motor_D;
+uint32_t status_motor_front, duty_motor_front, period_motor_front, period_motor_front_us, pulses_motor_front;
+
+double set_point_PID_front = 0;
+double output_PID_front = 0;
+double input_PID_front = 0;
+double PID_Kp_front = 1;
+double PID_Ki_front = 0;
+double PID_Kd_front = 0;
+
+PID PID_motor_front(&input_PID_front, &output_PID_front, &set_point_PID_front, PID_Kp_front, PID_Ki_front, PID_Kd_front, DIRECT);
 
 // Sentido de giro del motor
-#define z_f_pin_motor_D 9
+#define z_f_pin_motor_back 7
 // Pulsos de velocidad de salida
-//#define signal_pin_motor_D 10
+//#define signal_pin_motor_back 5
 // Enable control
-#define el_pin_motor_D 12
+#define el_pin_motor_back 4
 
-// Definiciones motor T(Tracero)
+// Definiciones motor back(Tracero)
 // Defining pwm object using pin 6, pin PC24 mapped to pin 6 on the DUE
 // This object uses PWM channel 0
-arduino_due::pwm_lib::pwm<arduino_due::pwm_lib::pwm_pin::PWML7_PC24> pwm_motor_T;
+arduino_due::pwm_lib::pwm<arduino_due::pwm_lib::pwm_pin::PWML7_PC24> pwm_motor_back;
 
-int salida_pwm_motor_T = 0;
-int entrada_analogica_motor_T = 0;
+int salida_pwm_motor_back = 0;
+int entrada_analogica_motor_back = 0;
 
 capture_tc6_declaration(); // TC0 and channel 1 pin pin digital 5
-auto& capture_motor_T = capture_tc6;
+auto& capture_motor_back = capture_tc6;
 
-uint32_t status_motor_T, duty_motor_T, period_motor_T, period_motor_T_us, pulses_motor_T;
+uint32_t status_motor_back, duty_motor_back, period_motor_back, period_motor_back_us, pulses_motor_back;
 
-// Sentido de giro del motor
-#define z_f_pin_motor_T 7
-// Pulsos de velocidad de salida
-//#define signal_pin_motor_T 5
-// Enable control
-#define el_pin_motor_T 4
+double set_point_PID_back = 0;
+double output_PID_back = 0;
+double input_PID_back = 0;
+double PID_Kp_back = 1;
+double PID_Ki_back = 0;
+double PID_Kd_back = 0;
+
+PID PID_motor_back(&input_PID_back, &output_PID_back, &set_point_PID_back, PID_Kp_back, PID_Ki_back, PID_Kd_back, DIRECT);
 
 byte cantidad_sensores;
 byte cantidad_actuadores;
@@ -90,6 +111,8 @@ int pin_RST_RS485 = 10;
 // Modo-funcion
 byte modo = 0;
 byte estado_debug = 0;
+
+
 
 //*************************************************************************************
 //********** SERVICIO DE INTERRUPCION  ISR(SERIAL_RX)   *********************
@@ -142,36 +165,44 @@ void serialEvent3() {
 //*************************************************************************************
 void setup() {
   // Initialization of capture objects
-  capture_motor_D.config(CAPTURE_TIME_WINDOW);
-  capture_motor_T.config(CAPTURE_TIME_WINDOW);
+  capture_motor_front.config(CAPTURE_TIME_WINDOW);
+  capture_motor_back.config(CAPTURE_TIME_WINDOW);
   // Put your setup code here, to run once:
   // Seteado a 10Khz para probar
-  pwm_motor_D.start(PWM_PERIODO_US, 1000);
-  pwm_motor_T.start(PWM_PERIODO_US, 1000);
+  pwm_motor_front.start(PWM_PERIODO_US, 1000);
+  pwm_motor_back.start(PWM_PERIODO_US, 1000);
 
-  pinMode(z_f_pin_motor_D, OUTPUT);
-  //pinMode(signal_pin_motor_D, INPUT);
-  pinMode(el_pin_motor_D, OUTPUT);
+  pinMode(z_f_pin_motor_front, OUTPUT);
+  //pinMode(signal_pin_motor_front, INPUT);
+  pinMode(el_pin_motor_front, OUTPUT);
 
-  pinMode(z_f_pin_motor_T, OUTPUT);
-  //pinMode(signal_pin_motor_T, INPUT);
-  pinMode(el_pin_motor_T, OUTPUT);
+  pinMode(z_f_pin_motor_back, OUTPUT);
+  //pinMode(signal_pin_motor_back, INPUT);
+  pinMode(el_pin_motor_back, OUTPUT);
 
   pinMode(pin_RST_RS485, OUTPUT);
   // desactiva el modo transmision en el conversor RS485
   digitalWrite(pin_RST_RS485, LOW);
 
-  pwm_motor_D.set_duty(0);
-  pwm_motor_T.set_duty(0);
-
+  pwm_motor_front.set_duty(0);
+  pwm_motor_back.set_duty(0);
+  #if (DEBUG_MODE) 
+    Serial.begin(115200);
+  #endif
   //Serial1.begin(1312500);
   Serial3.begin(256000);
   //Serial3.begin(115200);
   pinMode(30, OUTPUT);
-  // Ventana de tiempo 1mS
+  // Ventana de tiempo 10 mS
   Timer0.start(10000);
   // Interrupcion timer 1
   Timer0.attachInterrupt(funcion_t1);
+  //turn the PID on
+  PID_motor_front.SetMode(AUTOMATIC);
+  //turn the PID on
+  PID_motor_back.SetMode(AUTOMATIC);
+  PID_motor_front.SetSampleTime(10);// in ms
+  PID_motor_back.SetSampleTime(10);// in ms
 }//----------------   FIM DO SETUP Y PARAMETROS -----------------------------
 
 //*************************************************************************************
@@ -185,11 +216,62 @@ void loop() {
       // Ejemplo: espejo en dato[0]
       sensor[0] = actuador[0]; 
 
-      salida_pwm_motor_D = map(actuador[1], 0, 100, 0, PWM_PERIODO_US);
-      pwm_motor_D.set_duty(salida_pwm_motor_D);
-      salida_pwm_motor_T = map(actuador[2], 0, 100, 0, PWM_PERIODO_US);
-      pwm_motor_T.set_duty(salida_pwm_motor_T);
-    
+      set_point_PID_front = actuador[1]; // cm/s
+
+
+      if ((PID_Kp_front != actuador[2]) || (PID_Kp_front != actuador[3]) || (PID_Kp_front != actuador[4]) ){
+        PID_Kp_front = actuador[2];
+        PID_Ki_front = actuador[3];
+        PID_Kd_front = actuador[4];
+        PID_motor_front.SetTunings(PID_Kp_front, PID_Ki_front, PID_Kd_front);
+      }
+
+      set_point_PID_back = actuador[5]; // cm/s
+
+
+      if ((PID_Kp_back != actuador[6]) || (PID_Kp_back != actuador[7]) || (PID_Kp_back != actuador[8]) ){
+        PID_Kp_back = actuador[6];
+        PID_Ki_back = actuador[7];
+        PID_Kd_back = actuador[8];
+        PID_motor_back.SetTunings(PID_Kp_back, PID_Ki_back, PID_Kd_back);
+      }
+
+      status_motor_front = capture_motor_front.get_duty_period_and_pulses(duty_motor_front, period_motor_front, pulses_motor_front);
+      period_motor_front_us = period_motor_front/(42);
+      input_PID_front = metros_en_us / period_motor_front_us;
+      sensor[1] = input_PID_front;
+
+      status_motor_back = capture_motor_back.get_duty_period_and_pulses(duty_motor_back, period_motor_back, pulses_motor_back);
+      period_motor_back_us = period_motor_back/(42);
+      input_PID_back = metros_en_us / period_motor_back_us;
+      sensor[2] = input_PID_back;
+      
+      // PID calculation and command
+      PID_motor_front.Compute();
+      salida_pwm_motor_front = map(output_PID_front, 0, MAX_VEL_CM_S, 0, PWM_PERIODO_US);
+      pwm_motor_front.set_duty(salida_pwm_motor_front);
+      
+      PID_motor_back.Compute();
+      salida_pwm_motor_back = map(output_PID_back, 0, MAX_VEL_CM_S, 0, PWM_PERIODO_US);
+      pwm_motor_back.set_duty(salida_pwm_motor_back);
+
+
+      #if (DEBUG_MODE) 
+        Serial.println("El valor de los encoder en cm por s es :");
+        Serial.print("el motor A : ");
+        Serial.println(sensor[1]);
+        Serial.print("el motor B : ");
+        Serial.println(sensor[2]);
+        Serial.print("Metros en ms motor : ");
+        Serial.println(metros_en_us);
+        Serial.print("PID output motor front in cm/s : ");
+        Serial.println(output_PID_front);
+        Serial.print("PID output motor back in cm/s : ");
+        Serial.println(output_PID_back);
+      #endif
+      capture_motor_front.config(CAPTURE_TIME_WINDOW);
+      capture_motor_back.config(CAPTURE_TIME_WINDOW);
+      
     }
     // MODO = 0 --> STOP
     if (modo == 0) {
@@ -202,15 +284,7 @@ void loop() {
   }
   // Cada xxx ms ejecuta este codigo
   if (evento_tarea_1 == 1) {      
-      status_motor_D = capture_motor_D.get_duty_period_and_pulses(duty_motor_D, period_motor_D, pulses_motor_D);
-      period_motor_D_us = period_motor_D/(42);
-      sensor[1] = metros_en_us / period_motor_D_us;
-      capture_motor_D.config(CAPTURE_TIME_WINDOW);
-      status_motor_T = capture_motor_T.get_duty_period_and_pulses(duty_motor_T, period_motor_T, pulses_motor_T);
-      period_motor_T_us = period_motor_T/(42);
-      sensor[2] = metros_en_us / period_motor_T_us;
-      capture_motor_T.config(CAPTURE_TIME_WINDOW);
-      evento_tarea_1  = 0;
+    evento_tarea_1  = 0;
   }
   //*************************************************************************************
   //*************** EVENTO DETECCION DE TRAMA   *************************************
@@ -285,7 +359,7 @@ void loop() {
 }//----------------   FIM MAIN ---------------------------------
 
 //*************************************************************************************
-//****************** XXX  INTERRUPCION TIMER DE 1ms  XXX  *****************************
+//****************** XXX  INTERRUPCION TIMER DE 10 ms  XXX  *****************************
 //*************************************************************************************
 void funcion_t1() {
   evento_control = 1;
